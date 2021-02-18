7aaebfca32f398a4e4c219045f1e8f1bbf8b5ee7
Zack <rubinzack40@gmail.com>
2021-02-18 01:42:28 -0500

    bot initialization

---
+++ bot.py
@@ -0,0 +1,26 @@
+import tkn
+import discord
+
+TOKEN = tkn.tkn
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
