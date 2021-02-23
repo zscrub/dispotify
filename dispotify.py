cfb0991b64de6caabfa89582180c6ffd65502c9d
Zack <rubinzack40@gmail.com>
2021-02-23 02:01:35 -0500

    authentication is complete

--- dispotify.py
+++ dispotify.py
@@ -1,5 +1,5 @@
+import tkn
 import spotipy
-import util
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
 
@@ -7,7 +7,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 wkndAlbums = []
 theWkndURI = 'spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-token = util.prompt_for_user_token(username, scope, redirect_uri = 'https://example.com/callback/')
+token = spotipy.util.prompt_for_user_token(client_id= tkn.SPOTIPY_CLIENT_ID, client_secret= tkn.SPOTIPY_CLIENT_SECRET, redirect_uri= 'http://localhost:8888/callback')
 
 results = spotify.artist_albums(theWkndURI, album_type='album')
 albums = results['items']
